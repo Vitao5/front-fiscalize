048d6e7a8202baac1cb1d6d07a93314acb611451
Victor Gabriel <vitinmoreira1908@icloud.com>
2026-08-16 11:08:13 -0300

    implementação CSP

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,26 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://assets.pluggy.ai;
+  font-src 'self' data:;
+  connect-src 'self' http://localhost:3002 https://*.pluggy.ai;
+  frame-src 'self' https://connect.pluggy.ai;
+  frame-ancestors 'none';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+`.replace(/\s{2,}/g, ' ').trim();
+
 const nextConfig: NextConfig = {
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
+          { key: 'Content-Security-Policy', value: cspHeader },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'X-Frame-Options', value: 'DENY' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
@@ -24,4 +39,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default nextConfig
