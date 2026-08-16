8565502dc6460f7c69d6821351aa0b1cfcb50d72
Victor Gabriel <vitinmoreira1908@icloud.com>
2026-08-16 11:04:14 -0300

    proteção contra XSS e clickjacking

--- src/comum-functions.tsx
+++ src/comum-functions.tsx
@@ -1,19 +1,5 @@
 'use client';
 
-import { jwtDecode } from "jwt-decode";
-
-export default function getUserIdFromToken() {
-
-  const tokenUser = localStorage.getItem("user")
-  if (!tokenUser) throw new Error("Usuário não autenticado");
-  const token = JSON.parse(tokenUser).token
-  
-  if (!token) throw new Error("Usuário não autenticado");
-  
-  const decoded = jwtDecode<any>(token);
-  return decoded.id;
-}
-
 export function formatBRLInput(value: string): string {
   const digits = value.replace(/\D/g, "");
   if (!digits) return "";
